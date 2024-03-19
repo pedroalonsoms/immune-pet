764576d567a2dd105909da8f415ce645a6d08a2c
pedroalonsoms <pedro123ben10@gmail.com>
2024-03-19 10:34:08 -0600

    Initial Commit

---
+++ immune-pet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  immune-pet
+//
+//  Created by Pedro Alonso Moreno Salcedo on 19/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ immune-pet/immune_petApp.swift
@@ -0,0 +1,17 @@
+//
+//  immune_petApp.swift
+//  immune-pet
+//
+//  Created by Pedro Alonso Moreno Salcedo on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct immune_petApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
