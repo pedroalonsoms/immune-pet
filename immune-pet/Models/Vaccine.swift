f6d201087c88af47d276ec7ca84a872534c63ba1
pau <a01383351@tec.mx>
2024-03-20 10:10:32 -0600

    hola pau

--- immune-pet/Models/Vaccine.swift
+++ immune-pet/Models/Vaccine.swift
@@ -5,4 +5,12 @@
 //  Created by Paulina Covarrubias on 20/03/24.
 //
 
+
 import Foundation
+
+struct Vaccine:Codable, Identifiable{
+    let id: String
+    let name: String
+    let description: String
+    let date: TimeInterval
+}
--- immune-pet/Views/NewVaccineView.swift
+++ immune-pet/Views/NewVaccineView.swift
@@ -6,13 +6,39 @@
 //
 
 import SwiftUI
-
 struct NewVaccineView: View {
+    @StateObject var viewModel = NewVaccineViewModel()
+    @Binding var newVaccineDone: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center){
+            Form{
+                TextField("Name", text: $viewModel.title)
+                TextField("Description", text: $viewModel.description)
+                DatePicker("Date to be administered", selection: $viewModel.date)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                    .accentColor(.orange)
+                // botón para guardar cambios
+                Button(action:{
+                    viewModel.save() // llamamos a la función de save (ver NewVaccineViewModel)
+                    newVaccineDone = false
+                })
+                {
+                    VStack(alignment: .center){
+                        Text("ADD")
+                            .foregroundColor(.white)
+                            .padding(5) // para darle espacio al texto
+                    }
+                    .background(Color.orange)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/) // default redondear esquinas
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/) // para centrarlo y que ocupe todo el espacio ... https://sarunw.com/posts/how-to-make-swiftui-view-fill-container-width-and-height/
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }
+            }
+            .scrollContentBackground(.hidden) //para que el fondo se aplicara: https://stackoverflow.com/questions/57254461/how-to-change-the-background-color-for-a-form-in-swiftui#:~:text=You%20can%20use%20UITableView.,s%20background%20color%20to%20red.
+            .padding()
+            .background(Image("fondo")
+                .resizable()) //para la imagen usé esto: https://stackoverflow.com/questions/59102889/how-to-add-background-image-to-complete-view-in-swiftui
+        }
     }
-}
-
-#Preview {
-    NewVaccineView()
 }
