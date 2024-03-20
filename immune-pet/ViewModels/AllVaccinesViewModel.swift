0653e79057a3ce3088e378fb988d2e5a745c5065
pau <a01383351@tec.mx>
2024-03-20 09:52:07 -0600

    NewChanges

---
+++ immune-pet/ViewModels/AllVaccinesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AllVaccinesViewModel.swift
+//  immune-pet
+//
+//  Created by Paulina Covarrubias on 20/03/24.
+//
+
+import Foundation
---
+++ immune-pet/Views/AllVaccinesView.swift
@@ -0,0 +1,18 @@
+//
+//  AllVaccinesView.swift
+//  immune-pet
+//
+//  Created by Paulina Covarrubias on 20/03/24.
+//
+
+import SwiftUI
+
+struct AllVaccinesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllVaccinesView()
+}
